f7b7ee54f3b15ad1143ee0150d06a8da387c04b0
Christian Wach <needle@haystack.co.uk>
2018-04-29 15:29:50 +0100

    whitespace

--- assets/includes/site_banner.php
+++ assets/includes/site_banner.php
@@ -5,13 +5,13 @@
 
 
 
-<?php include( get_template_directory().'/assets/includes/network.php' ); ?>
+<?php include( get_template_directory() . '/assets/includes/network.php' ); ?>
 
 
 
 <div id="splash">
 
-<a href="/2018/files/2017/03/hijab-girl-signs-ball.jpg"><img src="/2018/files/2017/03/hijab-girl-signs-ball-200x150.jpg" alt="Girl signs The Ball" title="Girl signs The Ball" width="200" height="150" class="alignnone size-medium wp-image-122" /></a>
+<a href="/2018/files/2017/03/hijab-girl-signs-ball.jpg"><img src="/2018/files/2017/03/hijab-girl-signs-ball.jpg" alt="Girl signs The Ball" title="Girl signs The Ball" width="200" height="150" class="alignnone size-medium wp-image-122" /></a>
 
 </div><!-- /splash -->
 
@@ -19,12 +19,12 @@
 
 <div id="banner_copy">
 
-<h2>Welcome to <?php bloginfo('title'); if( is_home() ) { echo ' blog'; } ?></h2>
+<h2>Welcome to <?php bloginfo( 'title' ); if( is_home() ) { echo ' blog'; } ?></h2>
 
 <p>The Ball 2018 kicked off from Battersea Park in London, where the first official game of football was played, and is travelling across Europe to the World Cup in Russia. The journey is showing how football can help displaced people find a new home and build new communities.</p>
 
-<?php if( !is_home() ) { ?>
-<p id="gotoblog"><a href="/2018/blog/">Read the blog &rarr;</a></p>
+<?php if ( ! is_home() ) { ?>
+	<p id="gotoblog"><a href="/2018/blog/">Read the blog &rarr;</a></p>
 <?php } ?>
 
 </div><!-- /banner_copy -->
@@ -43,10 +43,7 @@
 <?php
 
 // get page list, blank by default
-$pagelist = apply_filters(
-	'theball_pagelist',
-	''
-);
+$pagelist = apply_filters( 'theball_pagelist', '' );
 
 // did we get a page list?
 if ( $pagelist != '' ) {

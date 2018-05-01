4403b4ca1474b3fa35ce83ec6e49476ca8eab8ca
Christian Wach <needle@haystack.co.uk>
2018-05-01 09:18:04 +0100

    fix extra bracket error

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ get_header(); ?>
 					</div><!-- /post_header -->
 
 					<div class="entry clearfix">
-						<?php the_excerpt()); ?>
+						<?php the_excerpt(); ?>
 					</div>
 
 					<p class="postmetadata"><?php the_tags( 'Tags: ', ', ', '<br />' ); ?> Posted in <?php the_category( ', ' ) ?> | <?php comments_popup_link( 'No Comments &#187;', '1 Comment &#187;', '% Comments &#187;' ); ?></p>

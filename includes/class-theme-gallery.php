d986f9f5ee5b24d66a6dc658cec8af8058abff31
Christian Wach <needle@haystack.co.uk>
2021-03-07 18:26:28 +0000

    Refactor Part Two

---
+++ includes/class-theme-gallery.php
@@ -0,0 +1,152 @@
+<?php /*
+================================================================================
+2018 Child Theme Gallery Class
+================================================================================
+AUTHOR: Christian Wach <needle@haystack.co.uk>
+--------------------------------------------------------------------------------
+NOTES
+
+--------------------------------------------------------------------------------
+*/
+
+
+
+/**
+ * The Ball 2018 Gallery Shortcode Filter Class.
+ *
+ * A class that encapsulates the functionality required to replace the assets of
+ * a gallery with those from a different blog.
+ *
+ * @since 1.0.3
+ */
+class The_Ball_2018_Gallery_Filter {
+
+	/**
+	 * Target Site ID from which assets should be pulled.
+	 *
+	 * @since 1.0.3
+	 * @access public
+	 * @var bool $site_id Target Site ID from which assets should be pulled.
+	 */
+	public $site_id;
+
+	/**
+	 * Gallery filter flag.
+	 *
+	 * @since 1.0.3
+	 * @access public
+	 * @var bool $gallery_filter True if currently filtering a gallery, false otherwise.
+	 */
+	public $gallery_filter = false;
+
+
+
+	/**
+	 * Initialises this object.
+	 *
+	 * @since 1.0.3
+	 */
+	public function __construct() {
+
+		// Add a filter for the gallery shortcode.
+		add_filter( 'post_gallery', [ $this, 'gallery_shortcode' ], 1010, 2 );
+
+		// Add a filter for the above.
+		add_filter( 'comments_open', [ $this, 'media_comment_status' ], 10, 2 );
+
+	}
+
+
+
+	/**
+	 * Switch to target site for gallery shortcode assets.
+	 *
+	 * @since 1.0.3
+	 *
+	 * @param str $output The existing shortcode output.
+	 * @param array $attr The shortcode attributes.
+	 * @return str $output The modified shortcode output.
+	 */
+	public function gallery_shortcode( $output, $attr ) {
+
+		// Check for our custom attribute.
+		if ( empty( $attr['sof_site_id'] ) ) {
+			return $output;
+		}
+		if ( ! is_numeric( $attr['sof_site_id'] ) ) {
+			return $output;
+		}
+
+		// Set site ID.
+		$this->site_id = absint( $attr['sof_site_id'] );
+
+		// Set filter flag.
+		$this->gallery_filter = true;
+
+		// Prevent recursion.
+		remove_filter( 'post_gallery', [ $this, 'gallery_shortcode' ], 1010 );
+
+		// Switch to SOF eV site and rebuild shortcode.
+		switch_to_blog( $this->site_id );
+		$output = do_shortcode( '[gallery type="' . $attr['type'] . '" ids="' . $attr['ids'] . '"]' );
+		restore_current_blog();
+
+		/*
+		$e = new Exception;
+		$trace = $e->getTraceAsString();
+		error_log( print_r( [
+			'method' => __METHOD__,
+			'attr' => $attr,
+			'output' => $output,
+			'backtrace' => $trace,
+		], true ) );
+		*/
+
+		// Reset filter.
+		add_filter( 'post_gallery', [ $this, 'gallery_shortcode' ], 1010, 2 );
+
+		// Reset filter flag.
+		$this->gallery_filter = false;
+
+		// --<
+		return $output;
+
+	}
+
+
+
+	/**
+	 * Remove comments from media attachments.
+	 *
+	 * This is done in order to remove the comments on the JetPack Carousel Slides.
+	 * The class properties prevent
+	 *
+	 * @since 1.0.3
+	 *
+	 * @param bool $open The existing comment status.
+	 * @param int $post_id The numeric ID of the post.
+	 * @return bool $open The modified comment status.
+	 */
+	public function media_comment_status( $open, $post_id ) {
+
+		// Bail if not filtering a gallery.
+		if ( $this->gallery_filter === false ) {
+			return $open;
+		}
+
+		// Bail if site ID is not properly set.
+		if ( ! is_numeric( $this->site_id ) ) {
+			return $open;
+		}
+
+		// --<
+		return false;
+
+	}
+
+
+
+}
+
+
+
---
+++ includes/class-theme.php
@@ -0,0 +1,250 @@
+<?php /*
+================================================================================
+2018 Child Theme Class
+================================================================================
+AUTHOR: Christian Wach <needle@haystack.co.uk>
+--------------------------------------------------------------------------------
+NOTES
+
+--------------------------------------------------------------------------------
+*/
+
+
+
+/**
+ * The Ball Theme Class.
+ *
+ * A class that encapsulates this theme's functionality.
+ *
+ * @since 1.0.5
+ */
+class SOF_The_Ball_2018_Theme {
+
+	/**
+	 * Gallery Filter object.
+	 *
+	 * @since 1.0.5
+	 * @access public
+	 * @var object $gallery_filter The Gallery Filter object.
+	 */
+	public $gallery_filter;
+
+
+
+	/**
+	 * Initialises this object.
+	 *
+	 * @since 1.0.5
+	 */
+	public function __construct() {
+
+		// Listen for parent theme class to be loaded.
+		add_action( 'sof/theme/the_ball/loaded', [ $this, 'initialise' ] );
+
+	}
+
+
+
+	/**
+	 * Include files.
+	 *
+	 * @since 1.0.0
+	 */
+	public function initialise() {
+
+		// Include files.
+		$this->include_files();
+
+		// Set up objects and references.
+		$this->setup_objects();
+
+		// Register hooks.
+		$this->register_hooks();
+
+		/**
+		 * Broadcast that this instance is loaded.
+		 *
+		 * @since 1.0.5
+		 */
+		do_action( 'sof/theme/the_ball_2018/loaded' );
+
+	}
+
+
+
+	/**
+	 * Include files.
+	 *
+	 * @since 1.0.5
+	 */
+	public function include_files() {
+
+		// Only do this once.
+		static $done;
+		if ( isset( $done ) AND $done === true ) {
+			return;
+		}
+
+		// Include global scope Theme Functions.
+		include get_stylesheet_directory() . '/includes/functions-theme.php';
+
+		// Include class files.
+		include get_stylesheet_directory() . '/includes/class-theme-gallery.php';
+
+		// We're done.
+		$done = true;
+
+	}
+
+
+
+	/**
+	 * Set up this plugin's objects.
+	 *
+	 * @since 1.0.5
+	 */
+	public function setup_objects() {
+
+		// Only do this once.
+		static $done;
+		if ( isset( $done ) AND $done === true ) {
+			return;
+		}
+
+		// Initialse objects.
+		$this->gallery_filter = new The_Ball_2018_Gallery_Filter();
+
+		// We're done.
+		$done = true;
+
+	}
+
+
+
+	/**
+	 * Register WordPress hooks.
+	 *
+	 * @since 1.0.5
+	 */
+	public function register_hooks() {
+
+		// Set up this theme's defaults.
+		add_action( 'after_setup_theme', [ $this, 'theme_setup' ] );
+
+		// Add CSS and JS with high priority so they are late in the queue.
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_styles' ], 1005 );
+
+		// Filter the image of The Ball.
+		add_filter( 'theball_image', [ $this, 'theball_image_filter' ] );
+
+		// Filter the Supporters file.
+		add_filter( 'theball_supporters', [ $this, 'supporters_file_filter' ] );
+
+		// Filter the Team Members array.
+		//add_filter( 'theball_team_members', [ $this, 'team_members_filter' ] );
+
+	}
+
+
+
+	/**
+	 * Augment the Base Theme's setup function.
+	 *
+	 * @since 1.0.0
+	 */
+	public function theme_setup() {
+
+		/*
+		 * Make theme available for translation.
+		 * Translations can be added to the /languages/ directory of the child theme.
+		 */
+		load_theme_textdomain(
+			'theball2018',
+			get_stylesheet_directory() . '/languages'
+		);
+
+	}
+
+
+
+	/**
+	 * Add child theme's CSS file(s).
+	 *
+	 * @since 1.0.0
+	 */
+	public function enqueue_styles() {
+
+		// Enqueue file.
+		wp_enqueue_style(
+			'theball2018_css',
+			get_stylesheet_directory_uri() . '/assets/css/style-overrides.css',
+			[ 'theball_screen_css' ],
+			THEBALL2018_VERSION, // Version.
+			'all' // Media.
+		);
+
+	}
+
+
+
+	/**
+	 * Override image of The Ball.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param str $default The existing markup for the image file.
+	 * @return str $default The modified markup for the image file.
+	 */
+	public function theball_image_filter( $default ) {
+
+		// Ignore default and set our own.
+		return '<a href="' . get_home_url( null, '/' ) . '" title="' . __( 'Home', 'theball2018' ) . '" class="ball_image">' .
+				'<img src="' . get_stylesheet_directory_uri() . '/assets/images/interface/the_ball_2018_200_sq.png" ' .
+					 'alt="' . esc_attr( __( 'The Ball 2018', 'theball2018' ) ) . '" ' .
+					 'title="' . esc_attr( __( 'The Ball 2018', 'theball2018' ) ) . '" ' .
+					 'style="width: 100px; height: 100px;" ' .
+					 'id="the_ball_header" />' .
+				'</a>' ;
+
+	}
+
+
+
+	/**
+	 * Override supporters footer template file.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param str $default The default path to the template file.
+	 * @return str $default The modified path to the template file.
+	 */
+	public function supporters_file_filter( $default ) {
+
+		// Override with 2018 file.
+		return get_stylesheet_directory() . '/assets/includes/supporters_2018.php';
+
+	}
+
+
+
+	/**
+	 * Override users in "Team" template file.
+	 *
+	 * @since 1.0.5
+	 *
+	 * @param array $users The default set of users.
+	 * @return array $users The modified set of users.
+	 */
+	public function team_members_filter( $default ) {
+
+		// 2018 users.
+		return [ 3, 5, 8, 7, 2, 4 ];
+
+	}
+
+
+
+}
+
+
+
